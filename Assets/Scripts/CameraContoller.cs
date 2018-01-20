24d3a76877ed61e10e2506fee5e139abec334147
yeasinmahi <yeasinmahi72@yahoo.com>
2018-01-21 00:39:00 +0600

    initial

---
+++ Assets/Scripts/CameraContoller.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraContoller : MonoBehaviour
+    {
+
+        public GameObject Player;
+
+        private Vector3 _offset;
+        void Start ()
+        {
+            _offset = transform.position - Player.transform.position;
+        }
+	
+        // Update is called once per frame
+        void LateUpdate ()
+        {
+            transform.position = Player.transform.position + _offset;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public float Speed;
+        private Rigidbody _rb;
+
+        void Start ()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        void FixedUpdate()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal,0f,moveVertical);
+
+            _rb.AddForce(movement*Speed);
+        }
+    }
+}
