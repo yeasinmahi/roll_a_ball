f8165ad01950d9605b31ea99adf01b53e7ad66cb
yeasinmahi <yeasinmahi72@yahoo.com>
2018-01-21 01:10:55 +0600

    added pickup objects

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Rotator : MonoBehaviour {
+        
+        void Update () {
+		transform.Rotate(new Vector3(15,30,45) * Time.deltaTime);
+        }
+    }
+}
